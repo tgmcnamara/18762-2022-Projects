39947631a974c74fca61e53d75081586b1a790da
Tim McNamara <tmcnama2@andrew.cmu.edu>
2022-01-20 09:51:44 -0500

    Renamed voltage source amplitude variable to be more descriptive

--- classes/VoltageSources.py
+++ classes/VoltageSources.py
@@ -4,11 +4,11 @@ from classes.Nodes import Nodes
 # from lib.stamping_functions import stamp_y_sparse, stamp_j_sparse
 
 class VoltageSources:
-    def __init__(self, name, vp_node, vn_node, amplitude, phase_deg, frequency_hz):
+    def __init__(self, name, vp_node, vn_node, amp_ph_ph_rms, phase_deg, frequency_hz):
         self.name = name
         self.vp_node = vp_node
         self.vn_node = vn_node
-        self.amplitude = amplitude
+        self.amp_ph_ph_rms = amp_ph_ph_rms
         self.phase_deg = phase_deg
         self.frequency_hz = frequency_hz
         # You are welcome to / may be required to add additional class variables   
