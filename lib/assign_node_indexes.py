c03bff93d9aa569b09ccb002b988da524df61095
Tim McNamara <tmcnama2@andrew.cmu.edu>
2022-01-20 10:10:39 -0500

    Updated gitignore and added lib folder and included files

---
+++ lib/assign_node_indexes.py
@@ -0,0 +1,5 @@
+
+def assign_node_indexes(devices):
+    node_index_counter = 0
+    # TODO
+    return node_index_counter
---
+++ lib/stamp.py
@@ -0,0 +1,7 @@
+
+
+def stamp_sparse():
+    pass
+
+def stamp_dense():
+    pass
