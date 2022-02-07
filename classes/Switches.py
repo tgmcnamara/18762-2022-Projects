a203ccb4dc6790f73c41ad1914a6f712a52b8517
Lucas Godshalk <lucasmg91@gmail.com>
2022-02-07 17:31:22 -0500

    Stab in the dark at switch stamping

--- classes/Switches.py
+++ classes/Switches.py
@@ -1,8 +1,6 @@
 import numpy as np
 from itertools import count
 from classes.Nodes import Nodes
-# from lib.stamping_functions import stamp_y_sparse, stamp_j_sparse
-import pickle
 
 class Switches:
     def __init__(self, name, from_node, to_node, t_open, t_close):
@@ -16,14 +14,27 @@ class Switches:
     def assign_node_indexes(self, nodeLookup: dict):
         self.from_index = nodeLookup[self.from_node]
         self.to_index = nodeLookup[self.to_node]
+
+        modified_index = len(nodeLookup)
+        nodeLookup[self.name] = modified_index
+        self.switch_index = modified_index
         
     def get_nodes_connections(self):
         return [self.from_node, self.to_node]
 
-    def stamp_sparse(self,):
-        pass
+    def stamp_dense(self, Y, J, v_previous, J_previous, runtime, timestep):
+        if runtime >= self.t_open and runtime <= self.t_close:
+            J[self.switch_index] = 0
 
-    def stamp_dense(self,):
+            Y[self.switch_index, self.from_index] = 1
+            Y[self.switch_index, self.to_index] = -1
+            pass
+        else:
+            # if its an open circuit, then there's no relationship between the nodes. just
+            # leave it un-stamped (unless there's another circuit in parallel).
+            pass
+
+    def stamp_sparse(self,):
         pass
 
     def stamp_open(self,):
