3cef1a89aa400f594817c207ce19ac4133065049
Lucas Godshalk <lucasmg91@gmail.com>
2022-03-23 21:07:42 -0400

    Add tests for transformer and shunt

--- run_solver.py
+++ run_solver.py
@@ -1,13 +1,9 @@
 from lib.Solve import solve
 from lib.settings import Settings
-from models.Branches import Branches
-from models.Buses import Bus
-from models.Generators import Generators
-from models.Slack import Slack
 from parsers.parser import parse_raw
 
 # path to the grid network RAW file
-casename = 'testcases/GS-4_prior_solution.RAW'
+casename = 'testcases/IEEE-14_prior_solution.RAW'
 
 raw_data = parse_raw(casename)
 
--- testcases/testall.py
+++ testcases/testall.py
@@ -6,27 +6,15 @@ from models.Branches import Branches
 from models.Buses import Bus
 from models.Generators import Generators
 from models.Loads import Loads
+from models.Shunts import Shunts
 from models.Slack import Slack
 from models.Transformers import Transformers
 
 class PowerFlowTests(unittest.TestCase):
-    def build_raw_data(self, devices):
-        raw_data = {}
-
-        raw_data['buses'] = [x for x in devices if isinstance(x, Bus)]
-        raw_data['generators'] = [x for x in devices if isinstance(x, Generators)]
-        raw_data['loads'] = [x for x in devices if isinstance(x, Loads)]
-        raw_data['branches'] = [x for x in devices if isinstance(x, Branches)]
-        raw_data['xfmrs'] = [x for x in devices if isinstance(x, Transformers)]
-        raw_data['slack'] = [x for x in devices if isinstance(x, Slack)]
-        raw_data['shunts'] = []
-
-        return raw_data
-
     def test_generator(self):
         b = Bus(1, 2, 1, 0, None)
         b.assign_nodes()
-        g = Generators(1, 200, 1, None, None, None, None, None, None, None, None)
+        generator = Generators(1, 200, 1, None, None, None, None, None, None, None, None)
 
         settings = Settings(debug=True, max_iters=30)
 
@@ -34,7 +22,7 @@ class PowerFlowTests(unittest.TestCase):
         J = [0, 0, 0]
         v_prev = [0.9819, -0.01673, 190]
 
-        g.stamp(Y, J, v_prev)
+        generator.stamp(Y, J, v_prev)
 
         matrix = Y.to_matrix().todense()
 
@@ -44,7 +32,7 @@ class PowerFlowTests(unittest.TestCase):
     def test_load(self):
         b = Bus(1, 2, 1, 0, None)
         b.assign_nodes()
-        g = Loads(1, 200, 100, None, None, None, None, None, None)
+        load = Loads(1, 200, 100, None, None, None, None, None, None)
 
         settings = Settings(debug=True, max_iters=30)
 
@@ -52,7 +40,7 @@ class PowerFlowTests(unittest.TestCase):
         J = [0, 0]
         v_prev = [0.9819, -0.01673]
 
-        g.stamp(Y, J, v_prev)
+        load.stamp(Y, J, v_prev)
 
         matrix = Y.to_matrix().todense()
 
@@ -74,6 +62,46 @@ class PowerFlowTests(unittest.TestCase):
         v_prev = None
 
         branch.stamp(Y, J, v_prev)
+
+        matrix = Y.to_matrix().todense()
+
+        print(matrix)
+        print(J)
+    
+    def test_shunt(self):
+        b = Bus(1, 2, 1, 0, None)
+        b.assign_nodes()
+        shunt = Shunts(1, 0, 19.0, 0, None, None, None, None, None, None, None)
+
+        settings = Settings(debug=True, max_iters=30)
+
+        Y = MatrixBuilder(settings)
+        J = [0, 0]
+        v_prev = [0.9819, -0.01673]
+
+        shunt.stamp(Y, J, v_prev)
+
+        matrix = Y.to_matrix().todense()
+
+        print(matrix)
+        print(J)
+
+    def test_transformer(self):
+        b1 = Bus(1, 1, 1, 0, None)
+        b1.assign_nodes()
+        b2 = Bus(2, 1, 1, 0, None)
+        b2.assign_nodes()
+
+        transformer = Transformers(1, 2, 1e-07, 0.20912, None, 0.978, 0.0, None, None, None)
+        transformer.assign_nodes()
+
+        settings = Settings(debug=True, max_iters=30)
+
+        Y = MatrixBuilder(settings)
+        J = [0, 0]
+        v_prev = None
+
+        transformer.stamp(Y, J, v_prev)
 
         matrix = Y.to_matrix().todense()
 
