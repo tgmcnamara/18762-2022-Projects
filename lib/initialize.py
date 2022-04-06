3b1cbcddc2844b2f32dd2591ac91a68a0c9704d0
Lucas Godshalk <lucasmg91@gmail.com>
2022-04-06 07:57:29 -0400

    Add flat start setting

--- lib/initialize.py
+++ lib/initialize.py
@@ -5,27 +5,26 @@ from models.Buses import Bus
 from models.Generators import Generators
 from models.Slack import Slack
 
+V_R_FLAT_STARt = 1
+V_I_FLAT_START = 0
+Q_FLAT_START = 1
 
 def initialize(Y_size, buses: List[Bus], generators: List[Generators], slacks: List[Slack], settings: Settings):
     Y = np.zeros(Y_size)
 
-    if settings.v_init == None:
-        for bus in buses:
-            (vr_idx, vr_init) = bus.get_Vr_init()
-            Y[vr_idx] = vr_init
+    for bus in buses:
+        (vr_idx, vr_init) = bus.get_Vr_init()
+        Y[vr_idx] = 1 if settings.flat_start else vr_init
 
-            (vi_idx, vi_init) = bus.get_Vi_init()
-            Y[vi_idx] = vi_init
-    else:
-        for idx in range(Y_size):
-            Y[idx] = settings.v_init
+        (vi_idx, vi_init) = bus.get_Vi_init()
+        Y[vi_idx] = 0 if settings.flat_start else vi_init
 
     for generator in generators:
-        Y[generator.bus.node_Q] = generator.Qinit / 100
+        Y[generator.bus.node_Q] = 1 if settings.flat_start else generator.Qinit / 100
 
     for slack in slacks:
-        Y[slack.slack_Ir] = slack.Pinit / 100
-        Y[slack.slack_Ii] = slack.Qinit / 100
+        Y[slack.slack_Ir] = 0 if settings.flat_start else slack.Pinit / 100
+        Y[slack.slack_Ii] = 0 if settings.flat_start else slack.Qinit / 100
 
     return Y
 
--- lib/settings.py
+++ lib/settings.py
@@ -1,15 +1,8 @@
 class Settings:
-    def __init__(self, tolerance = 1E-05, max_iters = 1000, limiting = False, v_init = None, debug = False, use_sparse = True) -> None:
-        """
-        Args:
-            tol (float): The chosen NR tolerance.
-            max_iters (int): The maximum number of NR iterations.
-            enable_limiting (bool): A flag that indicates if we use voltage limiting or not in our solver.
-        """
-
+    def __init__(self, tolerance = 1E-05, max_iters = 1000, limiting = False, debug = False, use_sparse = True, flat_start = False) -> None:
         self.tolerance = tolerance
         self.max_iters = max_iters
         self.limiting = limiting
-        self.v_init = v_init
         self.debug = debug
         self.use_sparse = use_sparse
+        self.flat_start = flat_start
