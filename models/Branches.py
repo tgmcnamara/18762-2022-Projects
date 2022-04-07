4374ab8836f26b3bb3d6f749eb00d73c713fbf2b
Lucas Godshalk <lucasmg91@gmail.com>
2022-04-07 16:16:17 -0400

    Misc

--- models/Branches.py
+++ models/Branches.py
@@ -21,19 +21,7 @@ class Branches:
                  rateA,
                  rateB,
                  rateC):
-        """Initialize a branch in the power grid.
-
-        Args:
-            from_bus (int): the bus number at the sending end of the branch.
-            to_bus (int): the bus number at the receiving end of the branch.
-            r (float): the branch resistance
-            x (float): the branch reactance
-            b (float): the branch susceptance
-            status (bool): indicates if the branch is online or offline
-            rateA (float): The 1st rating of the line.
-            rateB (float): The 2nd rating of the line
-            rateC (float): The 3rd rating of the line.
-        """
+                 
         self.id = self._ids.__next__()
 
         self.from_bus = _all_bus_key[from_bus]
