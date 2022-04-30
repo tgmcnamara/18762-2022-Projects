7c771a572f11fcac57dba5a84b290765f9da4340
Lucas Godshalk <lucasmg91@gmail.com>
2022-04-30 16:50:00 -0400

    Add IEEE-14 stressed and PEGASE-13659 cases as solver possibilities

--- run_solver.py
+++ run_solver.py
@@ -6,19 +6,24 @@ from lib.process_results import display_mat_comparison
 
 # path to the grid network RAW file
 #casename = 'testcases/GS-4_prior_solution.RAW'
-casename = 'testcases/GS-4_stressed.RAW'
+#casename = 'testcases/GS-4_stressed.RAW'
 #casename = 'testcases/IEEE-14_prior_solution.RAW'
 #casename = 'testcases/IEEE-118_prior_solution.RAW'
 #casename = 'testcases/ACTIVSg500_prior_solution_fixed.RAW'
 #casename = 'testcases/PEGASE-9241_flat_start.RAW'
+#casename = 'testcases/PEGASE-13659_flat_start.RAW'
+#casename = 'testcases/IEEE-14_stressed_1.RAW'
+casename = 'testcases/IEEE-14_stressed_2_fixed.RAW'
+
+print(f'Testcase: {casename.replace("testcases/", "").replace(".RAW", "")}')
 
 raw_data = parse_raw(casename)
 
-settings = Settings(max_iters=30, flat_start=False, infeasibility_analysis=True, tx_stepping=False)
+settings = Settings(max_iters=50, flat_start=True, infeasibility_analysis=True, tx_stepping=False, V_limiting=False)
 
 is_success, result = solve(raw_data, settings)
 
-result.display()
+result.display(verbose=False)
 
 if not is_success:
     print("SOLVER FAILED")
